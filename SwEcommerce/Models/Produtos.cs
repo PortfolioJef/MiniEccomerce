7fdb07eac35669cb713eccd7f70a2884b5de44a0
jefersonao <oliveira.jefersonaraujo@gmail.com>
2016-05-06 00:25:57 -0300

    Estruturação do Projeto.

---
+++ SwEcommerce/Models/Produtos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SwEcommerce.Models
+{
+    public class Produtos
+    {
+        public virtual Guid identificador { get; set; }
+        public virtual string nome { get; set; }
+        public virtual float preco { get; set; }
+        public virtual int? PromoAtrelada { get; set; }
+        public Produtos()
+        {
+
+        }
+    }
+}
