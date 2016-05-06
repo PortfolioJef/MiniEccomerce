817240852d4651c41a7f712fc9afaefbf539be02
jefersonao <oliveira.jefersonaraujo@gmail.com>
2016-05-06 01:21:59 -0300

    Alteração para banco de dados local em sql para não ser necessario criar server mysql para testes e criação da classe no fluent Hibernate.

---
+++ SwEcommerce/Helper/NHibernateHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate.Collection.Generic;
+using FluentNHibernate.Mapping;
+
+    public class NHibernateHelper
+    {
+        private static void CreateSessionFactory()
+        {
+            _sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008.ConnectionString(connectionString).ShowSql)
+            .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ServerData>())
+               .ExposeConfiguration(cfg => new SchemaExport(cfg).Create(false, false))
+            .BuildSessionFactory();
+        } 
+    }
+}
---
+++ SwEcommerce/Models/ProdutosMapData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentNHibernate.Mapping;  
+
+namespace SwEcommerce.Models
+{
+    public class ProdutosMapData : ClassMap<Produtos>
+    {
+        public ProdutosMapData()
+        {
+            Table("Produtos");
+            Id(id=>id.identificador,"identificador");
+            Map(x => x.nome, "nome");
+            Map(x => x.preco, "preco");
+                    }
+    }
+}
